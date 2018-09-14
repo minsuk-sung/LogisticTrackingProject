cf6e869cce7eca8a6002c11af3356bc8a7a93b15
Juneseok Byun <byunjuneseok@gmail.com>
2018-09-14 10:03:28 +0900

    //  error fix

--- ltp/manage.py
+++ ltp/manage.py
@@ -1,7 +1,8 @@
 import crawl_multi
 import googlegeo
+import apikeychain
 
 if __name__ == '__main__':
     #print("#CJ PATH CRAWLING TEST")
     print("#GOOGLE API KEY TEST")
-    googlegeo.main()
+    googlegeo.testcode(apikeychain.apikey)
