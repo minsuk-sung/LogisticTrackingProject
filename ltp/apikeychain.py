da24762c11f63ce02e266689ea68f087e0e0649f
InSoo <35443111+his9989@users.noreply.github.com>
2018-09-15 11:23:26 +0900

    Add files via upload

--- ltp/apikeychain.py
+++ ltp/apikeychain.py
@@ -1,5 +1,6 @@
 apikey = ['&key=AIzaSyD7gj21BMbRtNM7g2eppYav3JTMJfysvkE',
-          '&key=AIzaSyDeBDkD0R8QZhpc_tOOwpKy6JZCGzDkJfc']
+          '&key=AIzaSyDeBDkD0R8QZhpc_tOOwpKy6JZCGzDkJfc',
+          '&key=AIzaSyDqXAt5bhtG9rJsunK979EAFkWROaanU9E']
 
 apikey0 = apikey[0]
 apikey1 = apikey[1]
