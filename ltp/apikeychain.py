2b98329a5ce49c25aa710c7447eec67fa87c9a86
Juneseok Byun <byunjuneseok@gmail.com>
2018-09-18 19:19:26 +0900

    It's our secret :)

--- ltp/apikeychain.py
+++
@@ -1,7 +0,0 @@
-apikey = ['&key=AIzaSyD7gj21BMbRtNM7g2eppYav3JTMJfysvkE',
-          '&key=AIzaSyDeBDkD0R8QZhpc_tOOwpKy6JZCGzDkJfc',
-          '&key=AIzaSyDqXAt5bhtG9rJsunK979EAFkWROaanU9E']
-
-apikey0 = apikey[0]
-apikey1 = apikey[1]
-apikey2 = apikey[2]
